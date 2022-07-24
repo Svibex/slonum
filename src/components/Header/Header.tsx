0c8954d85c7c93e71e6e44cc336f72fb4c56b308
Svibex <future.science@yandex.ru>
2022-07-24 20:11:19 +0300

    feat: add header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
+    const logo = require("../../images/header/logo.png") as string;
+
     return (
         <header className="header">
             <div className="header__container">
+                <div className="header_logo">
+                    <img src={logo} alt="logo"/>
+                    <div className="header__title">
+                        <p className="header__title-text1">Слон</p>
+                        <p className="header__title-text2">УМ</p>
+                    </div>
+                </div>
+                <button>Войти</button>
             </div>
         </header>
     );
