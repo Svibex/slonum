e5bacb74460d2406f9a162129426dbc04af9ca41
Svibex <future.science@yandex.ru>
2022-07-24 19:33:39 +0300

    feat: add styles & fonts

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__container">
+            </div>
+        </header>
+    );
+};
+
+export default Header;
