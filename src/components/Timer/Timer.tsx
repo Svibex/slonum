461fde43afe14388e76d3c1475a444a218580304
Svibex <future.science@yandex.ru>
2022-07-26 23:24:44 +0300

    feat: complete functional in Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -6,27 +6,36 @@ const Timer = () => {
     const [timerDays, setTimerDays] = useState("00");
     const [timerHours, setTimerHours] = useState("00");
     const [timerMinutes, setTimerMinutes] = useState("00");
+    const [timerSeconds, setTimerSeconds] = useState("00");
 
-    let interval:{current: NodeJS.Timeout | null} = useRef(null);
+    let interval: { current: NodeJS.Timeout | null } = useRef(null);
+
+    let today = new Date();
+    let tomorrow = new Date(today.getTime() + 2 * (24 * 60 * 60 * 1000));
+    let dayTomorrow = tomorrow.getDate();
+    let monthTomorrow = tomorrow.getMonth() + 1;
+    let yearTomorrow = tomorrow.getFullYear();
+
+    function checkNumber(number: number): string {
+        return number < 10 ? `0${number}` : number.toString()
+    }
 
     const startTimer = () => {
-
-        const countdownDate = new Date("August 10 2022 00:00:00").getTime();
-        console.log(countdownDate)
+        const countdownDate = new Date(`${monthTomorrow} ${dayTomorrow} ${yearTomorrow}`).getTime();
 
         interval.current = setInterval(() => {
             const now = new Date().getTime();
             const distance = countdownDate - now;
 
-            const days = (Math.floor(distance/(1000*60*60*24))).toString();
-            const hours = (Math.floor(distance%(1000*60*60*24)/(1000*60*60))).toString();
-            const minutes = (Math.floor(distance%(1000*60*60)/(1000*60))).toString();
+            const days = (Math.floor(distance / (1000 * 60 * 60 * 24)));
+            const hours = (Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+            const minutes = (Math.floor(distance % (1000 * 60 * 60) / (1000 * 60)));
+            const seconds = (Math.floor(distance % (1000 * 60) / (1000)));
 
-            if (distance>0) {
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-            }
+            setTimerDays(checkNumber(days));
+            setTimerHours(checkNumber(hours));
+            setTimerMinutes(checkNumber(minutes));
+            setTimerSeconds(checkNumber(seconds));
 
         }, 1000);
     };
@@ -34,6 +43,8 @@ const Timer = () => {
     useEffect(() => {
         startTimer();
     });
+
+    console.log(timerSeconds);
 
     return (
         <div className="timer">
