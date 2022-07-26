2e1a6cd9d549e89cb63845dffa45793e0664a1c6
Svibex <future.science@yandex.ru>
2022-07-26 18:03:17 +0300

    feat: add subscribe component

---
+++ src/components/Subscribe/Subscribe.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Subscribe.css";
+
+const Subscribe = () => {
+    return (
+        <section className="subscribe">
+            <div className="subscribe__container">
+                <h2 className="title">Подпишись на рассылку</h2>
+                <p>
+                    Обещаем присылать только самое важное и интересное :)
+                </p>
+                <form>
+                    <div>
+                        <input type="text" placeholder="Email"/>
+                        <button type="submit">Отправить</button>
+                    </div>
+                    <div>
+                        <input type="checkbox"/>
+                        <p>
+                            Отправляя данные я соглашаюсь с <a>Условиями конкурса</a> и <a>Политикой обработки
+                            данных</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Subscribe;
