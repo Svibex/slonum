e3464d581c470314a9bc08e82c0abc24abc1894c
Svibex <future.science@yandex.ru>
2022-07-27 01:27:03 +0300

    feat: update AnimatedSpace styles

--- src/components/AnimatedSpace/AnimatedSpace.tsx
+++ src/components/AnimatedSpace/AnimatedSpace.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import "./AnimatedSpace.css";
 
 const AnimatedSpace = () => {
-    const space = require("../../assets/image/animatedSpace/space.png") as string;
     const girl = require("../../assets/image/animatedSpace/girl.png") as string;
     const paperAirplanes = require("../../assets/image/common/paperAirplanes.png") as string;
 
     return (
         <div className="animatedSpace">
-            <img className="animatedSpace__space" src={space} alt="Космос"/>
             <img className="animatedSpace__girl" src={girl} alt="Девочка"/>
             <img className="animatedSpace__paperAirplanes" src={paperAirplanes} alt="Бумажные самолетики"/>
         </div>
