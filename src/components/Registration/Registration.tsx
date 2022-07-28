7888152539442dca575e2bff70850ad56580aca3
Svibex <future.science@yandex.ru>
2022-07-28 16:07:04 +0300

    feat: update registration block styles

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -4,8 +4,6 @@ import "./Registration.css";
 
 const Registration = () => {
     const bookmark = require("../../assets/image/registration/bookmark.png") as string;
-    const vk = require("../../assets/image/common/vk.png") as string;
-    const ok = require("../../assets/image/registration/ok.png") as string;
 
     return (
         <section className="registration">
@@ -29,10 +27,18 @@ const Registration = () => {
                         </p>
                     </div>
                     <div className="registration__socialMedias">
-                        <p className="registration__socialMedias_text">Получить скидку:</p>
+                        <p className="registration__socialMedias_text">
+                            Получить скидку:
+                        </p>
                         <div className="registration__icons">
-                            <img className="registration__icons_icon" src={vk} alt="Вконтакте"/>
-                            <img className="registration__icons_icon" src={ok} alt="Одноклассники"/>
+                            <div
+                                className="registration__icons_icon"
+                                id="registration__vk"
+                            />
+                            <div
+                                className="registration__icons_icon"
+                                id="registration__ok"
+                            />
                         </div>
                     </div>
                 </div>
--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -6,25 +6,30 @@ const RegistrationForm = () => {
 
     return (
         <div className="registrationForm">
-            <h2 className="title">Зарегистрируйтесь, чтобы участвовать</h2>
+            <h2 className="registrationForm__title title">Зарегистрируйтесь, чтобы участвовать</h2>
             <form className="registrationForm__container">
-                <input type="text" placeholder="ФИО ребёнка"/>
-                <input type="text" placeholder="Дата рождения"/>
-                <input type="text" placeholder="Город"/>
-                <input type="text" placeholder="Email"/>
-                <input type="text" placeholder="Пароль"/>
-                <div>
-                    <input type="checkbox"/>
-                    <p>
-                        Отправляя данные я соглашаюсь с
-                        <a href="/#">Условиями конкурса</a> и <a href="/#">Политикой обработки данных</a>
+                <input className="registrationForm__input" type="text" placeholder="ФИО ребёнка"/>
+                <input className="registrationForm__input" type="text" placeholder="Дата рождения"/>
+                <input className="registrationForm__input" type="text" placeholder="Город"/>
+                <input className="registrationForm__input" type="text" placeholder="Email"/>
+                <input className="registrationForm__input" type="text" placeholder="Пароль"/>
+                <div className="registrationForm__consent">
+                    <button className="registrationForm__checkbox"/>
+                    <p className="registrationForm__text">
+                        Отправляя данные я соглашаюсь с&nbsp;
+                        <a className="registrationForm__link" href="/#">
+                            Условиями конкурса
+                        </a> и&nbsp;
+                        <a className="registrationForm__link" href="/#">
+                            Политикой обработки данных
+                        </a>
                     </p>
-                    <div>
-                        <button type="submit">
-                            Участвовать
-                        </button>
-                        <img src={paperAirplanes} alt="Бумажные самолетики"/>
-                    </div>
+                </div>
+                <div className="registrationForm__buttonGroup">
+                    <button className="registrationForm__button" type="submit">
+                        Участвовать
+                    </button>
+                    <img className="registrationForm__paperAirplanes" src={paperAirplanes} alt="Бумажные самолетики"/>
                 </div>
             </form>
         </div>
