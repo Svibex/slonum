30c25b87aaa0f9a106a08aa717219c9faae6237e
Svibex <future.science@yandex.ru>
2022-07-31 22:05:38 +0300

    feat: update timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,57 +1,44 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import "./Timer.css";
 
 const Timer = () => {
     const colon = require("../../assets/image/timer/colon.png") as string;
-    const [timerDays, setTimerDays] = useState("00");
-    const [timerHours, setTimerHours] = useState("00");
-    const [timerMinutes, setTimerMinutes] = useState("00");
 
-    let interval: { current: NodeJS.Timeout | null } = useRef(null);
-
-    const nextDay = new Date();
-    const tomorrow = new Date(nextDay.getTime() + 2 * (24 * 60 * 60 * 1000));
-    const dayTomorrow = tomorrow.getDate();
-    const monthTomorrow = tomorrow.getMonth() + 1;
-    const yearTomorrow = tomorrow.getFullYear();
+    const tomorrow = new Date(new Date().getTime() + 2 * (24 * 60 * 60 * 1000)),
+        dayTomorrow = tomorrow.getDate(),
+        monthTomorrow = tomorrow.getMonth() + 1,
+        yearTomorrow = tomorrow.getFullYear(),
+        now = new Date().getTime(),
+        countdownDate = new Date(`${monthTomorrow} ${dayTomorrow} ${yearTomorrow}`).getTime(),
+        distance = countdownDate - now,
+        days = checkNumber(Math.floor(distance / (1000 * 60 * 60 * 24))),
+        hours = checkNumber(Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))),
+        minutes = checkNumber(Math.floor(distance % (1000 * 60 * 60) / (1000 * 60))),
+        [timer, setTimer] = useState({days, hours, minutes})
 
     function checkNumber(number: number): string {
-        return number < 10 ? `0${number}` : number.toString()
+        return number < 10 ? `0${number}` : number.toString();
     }
 
-    const startTimer = () => {
-        const countdownDate = new Date(`${monthTomorrow} ${dayTomorrow} ${yearTomorrow}`).getTime();
-
-        interval.current = setInterval(() => {
-            const now = new Date().getTime();
-            const distance = countdownDate - now;
-
-            const days = (Math.floor(distance / (1000 * 60 * 60 * 24)));
-            const hours = (Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-            const minutes = (Math.floor(distance % (1000 * 60 * 60) / (1000 * 60)));
-
-            setTimerDays(checkNumber(days));
-            setTimerHours(checkNumber(hours));
-            setTimerMinutes(checkNumber(minutes));
-        }, 1000);
-    };
-
     useEffect(() => {
-        startTimer();
-    });
+        const interval = setInterval(() => {
+            setTimer(() => ({days, hours, minutes}));
+        }, 1000);
+        return () => clearInterval(interval)
+    }, [timer]);
 
     return (
         <div className="timer">
             <div className="timer__block">
                 <div className="timer__item">
-                    <p className="timer__item_date">{timerDays}</p>
+                    <p className="timer__item_date">{timer.days}</p>
                 </div>
                 <p className="timer__text">дней</p>
             </div>
             <img className="timer__colon" src={colon} alt="Двоеточие"/>
             <div className="timer__block">
                 <div className="timer__item">
-                    <p className="timer__item_date">{timerHours}</p>
+                    <p className="timer__item_date">{timer.hours}</p>
                 </div>
                 <p className="timer__text">часов</p>
                 <p className="timer__text timer__text_textAccent">по МСК</p>
@@ -59,7 +46,7 @@ const Timer = () => {
             <img className="timer__colon" src={colon} alt="Двоеточие"/>
             <div className="timer__block">
                 <div className="timer__item">
-                    <p className="timer__item_date">{timerMinutes}</p>
+                    <p className="timer__item_date">{timer.minutes}</p>
                 </div>
                 <p className="timer__text">минут</p>
             </div>
