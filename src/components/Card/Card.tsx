3afff2cc87660fa4c7623ad6b6602c1d0e027dbd
Svibex <future.science@yandex.ru>
2022-07-31 22:05:17 +0300

    feat: update variables

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ( props: {text:React.ReactElement, id:number} ) => {
+const Card = ( props: {text: React.ReactElement, id: number} ) => {
     const picture = require(`../../assets/image/card/${props.id}.png`) as string;
 
     return (
         <div className="card">
-            <img src={picture} alt="" />
+            <img src={picture} alt="Иконка" />
             <p className="card__text">
                 {props.text}
             </p>
--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -12,11 +12,11 @@ const Cover = () => {
                 <Header/>
                 <div className="cover__textGroup">
                     <p className="cover__info">От компании СлонУМ</p>
-                    <h1 className="cover__title">Всероссийский конкурс
-                        Детского рисунка</h1>
+                    <h1 className="cover__title">
+                        Всероссийский конкурс Детского рисунка
+                    </h1>
                     <p className="cover__text">Участвуйте в конкурсе и получайте призы в различных номинациях детского
-                        рисунка для всех
-                        возрастов</p>
+                        рисунка для всех возрастов</p>
                     <div className="cover__timerGroup">
                         <button className="cover__button">Участвовать</button>
                         <Timer/>
